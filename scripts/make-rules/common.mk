77b70f71aa8ecff3ccab8408442a7cf8830123c6
SeasonPilot <pilot1@foxmail.com>
2023-07-03 11:58:19 +0800

    chore: add comment for scripts/make-rules/common.mk

--- scripts/make-rules/common.mk
+++ scripts/make-rules/common.mk
@@ -2,6 +2,17 @@
 # Use of this source code is governed by a MIT style
 # license that can be found in the LICENSE file.
 
+#这段代码是一个Makefile脚本，它为构建和测试Go项目设置了各种变量和设置。
+ #脚本以指定许可证的一些注释开始，并将SHELL设置为/bin/bash。
+ #然后它包含一个公共生成文件，将common_SELF_DIR变量设置为包含当前生成文件的目录。
+ #接下来，它将设置一些尚未定义的环境变量。ROOT_DIR被设置为项目根目录（比当前makefile高两级）。OUTPUT_DIR设置为ROOT_DIR的一个子目录，称为_OUTPUT，TOOLS_DIR和TMP_DIR是OUTPUT_IR的子目录。如果这些目录不存在，脚本将创建这些目录。
+ #然后，脚本使用git标记设置项目的版本号，并检查git树是否脏（即是否有未提交的更改）。它还获取git提交哈希。
+ #下一节将为各种变量设置一些默认值，例如COVERAGE和PLATFORMS。它还将PLATFORM变量设置为当前操作系统和体系结构，并根据是否指定了PLATFORM，将IMAGE_PLAT设置为适当的值。
+ #还有各种命令行工具的设置，如FIND和XARGS，以及一些Makefile设置。
+ #该脚本将githook脚本复制到.git/hooks目录中，并将CERTIFICATES变量设置为需要证书的组件列表。
+ #最后，还有BLOCKER_TOOLS、CRITICAL_TOOLS和TRIVIAL_TOOLS的设置，这些设置指定了缺少工具的严重程度，并影响各种Makefile目标的成功。COMMA和SPACE变量也被定义用于列表构造。
+
+
 SHELL := /bin/bash
 
 # include the common make file
